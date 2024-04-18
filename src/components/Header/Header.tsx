fa2a22ce821477f83f6834a6ba6d493807a0b7de
Cass <hicass.dev@gmail.com>
2024-04-17 17:30:15 -0700

    Add fonts (#8)

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,8 +1,8 @@
 export default function Header() {
   return (
     <div className="flex flex-col items-center">
-      <h1>Mi Parcae, and then some</h1>
-      <h2>becuase sometimes it feels like im cheating fate</h2>
+      <h1 id="header-h1">MI PARCAE, AND THEN SOME</h1>
+      <h2 id="header-h2">becuase sometimes it feels like i'm cheating fate</h2>
     </div>
   );
 }
