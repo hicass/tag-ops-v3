da38790cc74b7cfef370b61ee8c1e08b6dc24bcb
hicass <cassidyjwalters@gmail.com>
2024-06-27 12:44:21 -0700

    Fix styling on Header component on Landing page for small window sizes

--- src/components/LandingPage/Header.tsx
+++ src/components/LandingPage/Header.tsx
@@ -10,8 +10,8 @@ const Header: FC = () => {
   return (
     <section className="w-full flex flex-col items-center my-12 md:h-96 sm:mb-40 sm:mt-24 lg:w-3/4">
       <h1 className="secret">Tag Operations</h1>
-      <div className="flex flex-col sm:flex-row items-center justify-center sm:mt-10 sm:gap-12 lg:w-full">
-        <h1 className="text-center sm:mt-6 text-4xl sm:text-7xl text-primary">
+      <div className="flex flex-col sm:flex-row items-center justify-center sm:mt-10 sm:gap-6 md:gap-12 sm:w-full">
+        <h1 className="text-center md:mt-6 text-4xl sm:text-6xl md:text-7xl text-primary">
           Simplify, Optimize, <br /> Scale with...
         </h1>
 
@@ -37,7 +37,7 @@ const Header: FC = () => {
         </motion.div>
       </div>
 
-      <p className="text-center text-lg w-2/3 sm:w-full  sm:-mt-4 md:text-3xl">
+      <p className="text-center text-lg w-2/3 sm:w-full md:-mt-4 sm:text-2xl md:text-3xl">
         Your tech forward, embedded, and flexible back office team!
       </p>
     </section>
