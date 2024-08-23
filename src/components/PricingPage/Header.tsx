3c51df1482a14486afdc227e5fdb94e22a2e425e
hicass <cassidyjwalters@gmail.com>
2024-08-23 12:47:36 -0700

    Add a Header/ Intro to the pricing page

---
+++ src/components/PricingPage/Header.tsx
@@ -0,0 +1,13 @@
+'use client';
+
+import { FC } from 'react';
+
+const Header: FC = () => {
+  return(
+    <div className="flex flex-col items-center">
+      <h1 className="text-3xl sm:text-4xl">Unlock the Full Potential of Your Operations</h1>
+    </div>
+  )
+}
+
+export default Header;
--- src/components/PricingPage/PricingPageContent.tsx
+++ src/components/PricingPage/PricingPageContent.tsx
@@ -1,9 +1,13 @@
 import { FC } from 'react';
 
+import Header from './Header';
+import Intro from './Intro/Intro';
+
 const PricingPageContent: FC = () => {
   return (
-    <main className="flex flex-col items-center overflow-hidden">
-      <h1>Pricing Page</h1>
+    <main className="flex flex-col items-center my-20 mx-40 gap-10">
+      <Header />
+      <Intro />
     </main>
   );
 }
