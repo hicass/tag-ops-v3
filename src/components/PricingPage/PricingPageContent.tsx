3830166219753280cdb47748c51d596be4be601c
hicass <cassidyjwalters@gmail.com>
2024-08-23 15:39:14 -0700

    Add quote to pricing page

--- src/components/PricingPage/PricingPageContent.tsx
+++ src/components/PricingPage/PricingPageContent.tsx
@@ -2,12 +2,14 @@ import { FC } from 'react';
 
 import Header from './Header';
 import Intro from './Intro/Intro';
+import Quote from './Quote';
 
 const PricingPageContent: FC = () => {
   return (
-    <main className="flex flex-col items-center my-20 mx-40 gap-10">
+    <main className="flex flex-col items-center my-20 gap-16">
       <Header />
       <Intro />
+      <Quote />
     </main>
   );
 }
