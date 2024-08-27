b800782ebb157f80a13afc6e56a0ae15f95f2c34
hicass <cassidyjwalters@gmail.com>
2024-08-26 18:02:19 -0700

    Update styling to include a blur if add ons are active

--- src/components/PricingPage/PricingPageContent.tsx
+++ src/components/PricingPage/PricingPageContent.tsx
@@ -1,21 +1,35 @@
-import { FC } from 'react';
+'use client';
+
+import { FC, useState } from 'react';
 
 import Header from './Header';
 import Intro from './Intro/Intro';
 import Quote from './Quote';
-import PricingCardSection from './PricingCardSection';
+import PricingCardSection from './PricingCardSection/PricingCardSection';
 import ClientsServed from '../ClientsServed/ClientsServed';
 
 const PricingPageContent: FC = () => {
+  const [pageBlur, setPageBlur] = useState(false);
+
   return (
-    <main className="flex flex-col items-center my-20 gap-16">
+    <main className="flex flex-col items-center my-20 gap-16 relative overflow-hidden">
       <Header />
       <Intro />
       <Quote />
-      <PricingCardSection />
+      <PricingCardSection pageBlur={pageBlur} setPageBlur={setPageBlur} />
       <ClientsServed />
+
+      {pageBlur && (
+        <div
+          className="w-full absolute top-0 left-0 bg-gradient-to-b from-transparent to-transparent h-full backdrop-filter backdrop-blur-[1.5px]"
+          style={{
+            backgroundImage:
+              'linear-gradient(to bottom, transparent, rgba(245, 238, 254, .40), transparent)',
+          }}
+        ></div>
+      )}
     </main>
   );
-}
+};
 
 export default PricingPageContent;
