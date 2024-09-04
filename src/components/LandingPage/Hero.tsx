4143cceb3ea91c445d233c6ec238fbd6e94f89dc
hicass <cassidyjwalters@gmail.com>
2024-09-04 13:51:42 -0700

    Remove CTA button from the Hero

--- src/components/LandingPage/Hero.tsx
+++ src/components/LandingPage/Hero.tsx
@@ -10,20 +10,18 @@ const Hero: FC = () => {
     <div className="flex flex-col items-center w-full">
       <h1 className="secret">Tag Operations</h1>
 
-      <div className="flex flex-col items-center text-center mt-14 sm:mt-24 mx-2 gap-4 sm:gap-0 sm:w-full sm:p-2 lg:w-full 2xl:w-1/2 2xl:my-36 sm:mb-14">
+      <div className="flex flex-col items-center text-center mt-14 sm:mt-32 mx-2 gap-4 sm:gap-0 sm:w-full sm:p-2 lg:w-full 2xl:w-1/2 2xl:my-36 sm:mb-14">
         <h1 className="flex text-h3 manrope-bold mx-0 sm:mx-0 sm:text-h1">
           Simplify, Optimize, Scale with...
         </h1>
 
-        <div className="w-full sm:w-2/3 2xl:w-5/6 relative">
+        <div className="w-full sm:w-2/3 2xl:w-full relative">
           <MainLogo />
         </div>
 
-        <p className="text-lg sm:text-h3 manrope-semibold text-center my-2 sm:mt-6 sm:mb-12 w-full">
+        <p className="text-lg sm:text-h3 manrope-semibold text-center my-2 sm:mt-6 sm:mb-2 w-full">
           Your trusted, embedded, and proactive team.
         </p>
-
-        <Button text={"Let's Talk!"} blue={true} href="/contact" />
       </div>
     </div>
   );
