9d4b58afc6a3d357f48b3ed42f402e8afc887beb
hicass <cassidyjwalters@gmail.com>
2024-09-03 18:22:04 -0700

    Update to have more responsive styling

--- src/components/Accents/WavyLine.tsx
+++ src/components/Accents/WavyLine.tsx
@@ -9,15 +9,16 @@ const WavyLine: FC<WavyLineProps> = ({ position }) => {
     <div className={`z-10 ${position}`}>
       <svg
         width="100vw"
-        height="169"
+        height="auto"
         viewBox="0 0 1440 169"
         fill="none"
         xmlns="http://www.w3.org/2000/svg"
+        preserveAspectRatio="none"
       >
         <path
           d="M-27 94.5C100.347 19.415 374.591 -21.007 746 94.5C1037 185 1321.5 140.5 1475.5 94.5"
           stroke="#A3DDFF"
-          strokeWidth="30"
+          strokeWidth="4%"
         />
       </svg>
     </div>
