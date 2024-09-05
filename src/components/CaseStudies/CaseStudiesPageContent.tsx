72b0532d9efc98338740c0202ece40e8a42539ce
hicass <cassidyjwalters@gmail.com>
2024-09-05 15:17:15 -0700

    Add the clients served section to the Case Studies page

--- src/components/CaseStudies/CaseStudiesPageContent.tsx
+++ src/components/CaseStudies/CaseStudiesPageContent.tsx
@@ -2,12 +2,14 @@ import { FC } from 'react';
 
 import Header from '@/components/CaseStudies/Header';
 import CaseStudyIntroCards from '@/components/CaseStudies/CaseStudyIntroCards';
+import ClientsServed from '../ClientsServed/ClientsServed';
 
 const CaseStudiesPageContent: FC = () => {
   return (
     <main className="flex flex-col items-center gap-10 relative overflow-hidden">
       <Header />
       <CaseStudyIntroCards />
+      <ClientsServed />
     </main>
   );
 }
