68b15c4af6ed3e9944db55ea02d79f7bba803c4a
hicass <cassidyjwalters@gmail.com>
2024-09-05 12:00:56 -0700

    Update to take a textSize prop to dynamically style the button text size

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -9,16 +9,19 @@ interface ButtonProps {
   href?: string;
   blue?: boolean;
   onClick?: React.MouseEventHandler<HTMLButtonElement>;
+  textSize?: string;
 }
 
-const Button: FC<ButtonProps> = ({ text, href, onClick, blue }) => {
+const Button: FC<ButtonProps> = ({ text, href, onClick, blue, textSize }) => {
   if (href) {
     return (
       <Link
         href={href}
         className={`${
           blue
-            ? 'bg-accent hover:bg-accent/80 hover:text-primary text-h4'
+            ? `bg-accent hover:bg-accent/80 hover:text-primary ${
+                textSize ? textSize : 'text-h4'
+              }`
             : 'bg-secondary hover:bg-secondary/70'
         } txt-lg kayak-bold px-4 py-[0.4rem] rounded-md w-fit`}
       >
