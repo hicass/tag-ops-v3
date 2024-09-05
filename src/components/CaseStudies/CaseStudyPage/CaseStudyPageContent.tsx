8d58bcf4787936018cb2bb697f141d5bb0ab53bb
hicass <cassidyjwalters@gmail.com>
2024-09-05 16:44:24 -0700

    Import and render Case Study page related components

--- src/components/CaseStudies/CaseStudyPage/CaseStudyPageContent.tsx
+++ src/components/CaseStudies/CaseStudyPage/CaseStudyPageContent.tsx
@@ -1,9 +1,15 @@
 'use client';
 
-import ClientsServed from '@/components/ClientsServed/ClientsServed';
-import { Testimonial } from '@/components/LandingPage/TestimonialsSection';
 import { FC } from 'react';
+
+import { Testimonial } from '@/components/LandingPage/TestimonialsSection';
+
 import Header from './Header';
+import SnapshotSection from './SnapshotSection';
+import ProblemSection from './ProblemSection';
+import ClientsServed from '@/components/ClientsServed/ClientsServed';
+import ResultsSection from './ResultsSection';
+import TestimonialSection from './TestimonialSection';
 
 export type CaseStudy = {
   title: string;
@@ -13,24 +19,24 @@ export type CaseStudy = {
   problem: string[];
   results: string[];
   testimonial: Testimonial;
-}
+};
 
-type Logo = {
+export type Logo = {
   logoSrc: string;
   logoAlt: string;
   logoWidth: number;
   logoHeight: number;
   logoLink: string;
-}
+};
 
-type Snapshot = {
+export type Snapshot = {
   industry: string;
   location: string;
   companySize: string;
   yearFounded: string;
   solution: string;
   description: string;
-}
+};
 
 interface CaseStudyPageContentProps {
   caseStudy: CaseStudy;
@@ -38,8 +44,12 @@ interface CaseStudyPageContentProps {
 
 const CaseStudyPageContent: FC<CaseStudyPageContentProps> = ({ caseStudy }) => {
   return (
-    <main className="flex flex-col items-center gap-10 relative overflow-hidden">
+    <main className="flex flex-col items-center gap-16 relative overflow-hidden">
       <Header title={caseStudy.title} topCards={caseStudy.topCards} />
+      <SnapshotSection snapshot={caseStudy.snapshot} logo={caseStudy.logo} />
+      <ProblemSection problem={caseStudy.problem} />
+      <ResultsSection results={caseStudy.results} />
+      <TestimonialSection testimonial={caseStudy.testimonial} />
       <ClientsServed />
     </main>
   );
