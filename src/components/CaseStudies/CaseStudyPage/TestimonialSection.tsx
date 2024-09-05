63a61813b365d7bafa55314c1eab3b6b31f0b8e3
hicass <cassidyjwalters@gmail.com>
2024-09-05 16:47:13 -0700

    Create TestimonialSection for the Case Study Page

---
+++ src/components/CaseStudies/CaseStudyPage/TestimonialSection.tsx
@@ -0,0 +1,38 @@
+'use client';
+
+import { FC } from 'react';
+
+import { motion } from 'framer-motion';
+
+import { Testimonial } from '@/components/LandingPage/TestimonialsSection';
+
+interface TestimonialProps {
+  testimonial: Testimonial;
+}
+
+const TestimonialSection: FC<TestimonialProps> = ({ testimonial }) => {
+  return (
+    <section className="w-fit flex flex-col items-start gap-6 mx-5 md:mx-0 md:w-2/3 2xl:w-1/2">
+      <h2>Testimonial</h2>
+
+      <motion.div
+        initial={{ opacity: 0 }}
+        viewport={{
+          amount: 0.2,
+        }}
+        whileInView={{ opacity: 1 }}
+        transition={{ duration: 0.3 }}
+        className="bg-accentlight rounded-xl p-4 drop-shadow"
+      >
+        <div>
+          <p className="txt-lg-bold">{testimonial.name}</p>
+          <p className="txt-md-semibold">{testimonial.title}</p>
+        </div>
+
+        <p className="pt-2 txt-rg">{testimonial.description}</p>
+      </motion.div>
+    </section>
+  );
+};
+
+export default TestimonialSection;
