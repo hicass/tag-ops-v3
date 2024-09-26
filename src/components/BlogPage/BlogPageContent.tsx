5f43fedf22e5b49a68306c026942f61b2f6b936d
hicass <cassidyjwalters@gmail.com>
2024-09-26 11:42:58 -0700

    Add Sanity to Blog Page: - Update typing and interfaces to match sanity - Change the dynamic routing from being id based to slug

--- src/components/BlogPage/BlogPageContent.tsx
+++ src/components/BlogPage/BlogPageContent.tsx
@@ -1,14 +1,54 @@
 import { FC } from 'react';
+import Link from 'next/link';
+
+import { PortableTextBlock } from '@portabletext/types';
 
 import Header from './Header';
 import BlogCards from './BlogCards';
 import ClientsServed from '../ClientsServed/ClientsServed';
 
-const BlogPageContent: FC = () => {
+interface Slug {
+  current: string;
+  _type: string;
+}
+
+interface BlogPost {
+  _id: string;
+  _updatedAt: string;
+  _createdAt: string;
+  _rev: string;
+  _type: 'blogPost';
+  title: string;
+  preview: string;
+  body: PortableTextBlock[];
+  author: string;
+  slug: Slug;
+  publishedAt: string;
+}
+
+export interface BlogPostsContentProps {
+  BlogPostsData: BlogPost[];
+}
+
+const BlogPageContent: FC<BlogPostsContentProps> = ({ BlogPostsData }) => {
+  console.log(BlogPostsData)
   return (
     <main className="flex flex-col items-center w-full gap-8 md:gap-10 relative">
       <Header />
-      <BlogCards />
+      {BlogPostsData[0] ? (
+        <BlogCards BlogPostsData={BlogPostsData} />
+      ) : (
+        <div className="flex flex-col justify-center items-center my-10">
+        {' '}
+        <p className='text-lg'>No Blog posts found...</p>
+        <Link
+          href="/"
+          className="mt-4 underline underline-offset-2 hover:text-primary"
+        >
+          Return to home
+        </Link>
+      </div>
+      )}
       <ClientsServed />
     </main>
   );
