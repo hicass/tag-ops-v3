024c0c5a8c54a1117c363f83e770c21abeee20f1
hicass <cassidyjwalters@gmail.com>
2024-09-26 11:47:06 -0700

    Remove un-needed Sanity schemas

--- src/sanity/schemaTypes/index.ts
+++ src/sanity/schemaTypes/index.ts
@@ -1,11 +1,9 @@
 import { type SchemaTypeDefinition } from 'sanity';
 
 import { blockContentType } from './blockContentType';
-import { categoryType } from './categoryType';
-import { postType } from './postType';
-import { authorType } from './authorType';
+import { blogPostType } from './blogPostType';
 import { landingPageType } from './landingPageType';
 
 export const schema: { types: SchemaTypeDefinition[] } = {
-  types: [blockContentType, categoryType, postType, authorType, landingPageType],
+  types: [blockContentType, blogPostType, landingPageType],
 };
